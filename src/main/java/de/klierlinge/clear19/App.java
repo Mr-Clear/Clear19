e735a141f14f3797207f463e7dceadf586ad772e
Thomas <daedalus-thomas@vorderklier.de>
2019-10-19 22:21:32 +0200

    Connect to LCD

--- src/main/java/de/klierlinge/clear19/App.java
+++ src/main/java/de/klierlinge/clear19/App.java
@@ -1,13 +1,35 @@
 package de.klierlinge.clear19;
 
-/**
- * Hello world!
- *
- */
+import java.awt.Color;
+import java.awt.Graphics;
+import java.io.IOException;
+
+import net.djpowell.lcdjni.AppletCapability;
+import net.djpowell.lcdjni.DeviceType;
+import net.djpowell.lcdjni.LcdConnection;
+import net.djpowell.lcdjni.LcdDevice;
+import net.djpowell.lcdjni.LcdRGBABitmap;
+import net.djpowell.lcdjni.Priority;
+import net.djpowell.lcdjni.SyncType;
+
 public class App
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws InterruptedException, IOException
     {
-        System.out.println("Hello World!");
+        try (LcdConnection con = new LcdConnection("HelloWorld", false, AppletCapability.getCaps(AppletCapability.QVGA), null, null);
+                LcdDevice device = con.openDevice(DeviceType.QVGA, null);
+                LcdRGBABitmap bmp = device.createRGBABitmap();)
+        {
+            final Graphics g = bmp.getGraphics();
+            g.setColor(Color.red);
+            g.fillRect(0, 0, 320, 240);
+            g.setColor(Color.white);
+            g.drawString("Hello World!", 100, 100);
+            g.dispose();
+            bmp.updateScreen(Priority.ALERT, SyncType.SYNC);
+            device.setForeground(true);
+            Thread.sleep(10000);
+        }
+        LcdConnection.deInit();
     }
 }
