a8f53e6f27518d678941644579216040f3970718
Thomas <git@thomas.klierlinge.de>
2020-01-21 14:43:52 +0100

    Show values from klierlinge

--- clear19/App/main_screen.py
+++ clear19/App/main_screen.py
@@ -1,6 +1,7 @@
 import dataclasses
-from datetime import timedelta
-from typing import Optional, List
+import json
+from datetime import timedelta, datetime
+from typing import Optional, List, Any, Dict
 
 from clear19.App import Global
 from clear19.App.screens import Screens
@@ -23,30 +24,30 @@ class MainScreen(Screen):
     def __init__(self, parent: AppWidget):
         super().__init__(parent, "Main")
 
-        lv2_3 = Line(self, Line.Orientation.VERTICAL)
-        lv2_3.rectangle = Rectangle(AnchoredPoint(self.width / 3 * 2, 0, Anchor.TOP_LEFT),
-                                  Size(lv2_3.preferred_size().width, self.height))
-        self.children.append(lv2_3)
+        self.lv2_3 = Line(self, Line.Orientation.VERTICAL)
+        self.lv2_3.rectangle = Rectangle(AnchoredPoint(self.width / 3 * 2, 0, Anchor.TOP_LEFT),
+                                         Size(self.lv2_3.preferred_size().width, self.height))
+        self.children.append(self.lv2_3)
 
-        d = TimeWidget(self, '%a %d.%m.%Y', h_alignment=TextWidget.HAlignment.CENTER)
-        d.rectangle = Rectangle(lv2_3.position(Anchor.TOP_RIGHT).anchored(Anchor.TOP_LEFT),
-                                lv2_3.position(Anchor.TOP_RIGHT) - self.position(Anchor.BOTTOM_RIGHT))
-        d.font = dataclasses.replace(d.font, bold=True)
-        d.fit_font_size()
-        d.set_height(d.preferred_size.height, VAnchor.TOP)
-        self.children.append(d)
+        self.date = TimeWidget(self, '%a %d.%m.%Y', h_alignment=TextWidget.HAlignment.CENTER)
+        self.date.rectangle = Rectangle(self.lv2_3.position(Anchor.TOP_RIGHT).anchored(Anchor.TOP_LEFT),
+                                        self.lv2_3.position(Anchor.TOP_RIGHT) - self.position(Anchor.BOTTOM_RIGHT))
+        self.date.font = dataclasses.replace(self.date.font, bold=True)
+        self.date.fit_font_size()
+        self.date.set_height(self.date.preferred_size.height, VAnchor.TOP)
+        self.children.append(self.date)
 
-        t = TimeWidget(self, '%H:%M:%S')
-        t.rectangle = Rectangle(d.position(Anchor.BOTTOM_LEFT).anchored(Anchor.TOP_LEFT) + Point(0, 2),
-                                d.position(Anchor.BOTTOM_LEFT) - self.position(Anchor.BOTTOM_RIGHT))
-        t.fit_font_size()
-        t.set_height(t.preferred_size.height, VAnchor.TOP)
-        self.children.append(t)
+        self.time = TimeWidget(self, '%H:%M:%S')
+        self.time.rectangle = Rectangle(self.date.position(Anchor.BOTTOM_LEFT).anchored(Anchor.TOP_LEFT) + Point(0, 2),
+                                        self.date.position(Anchor.BOTTOM_LEFT) - self.position(Anchor.BOTTOM_RIGHT))
+        self.time.fit_font_size()
+        self.time.set_height(self.time.preferred_size.height, VAnchor.TOP)
+        self.children.append(self.time)
 
-        lh1 = Line(self, Line.Orientation.HORIZONTAL)
-        lh1.rectangle = Rectangle(t.position(Anchor.BOTTOM_LEFT).anchored(Anchor.TOP_LEFT),
-                                  Size(self.width - lv2_3.left, lh1.preferred_size().height))
-        self.children.append(lh1)
+        self.lh1 = Line(self, Line.Orientation.HORIZONTAL)
+        self.lh1.rectangle = Rectangle(self.time.position(Anchor.BOTTOM_LEFT).anchored(Anchor.TOP_LEFT),
+                                       Size(self.width - self.lv2_3.left, self.lh1.preferred_size().height))
+        self.children.append(self.lh1)
 
         self.wetter_com = WetterCom('DE0008184003', Global.download_manager)
         self.weather_widgets = WeatherWidgets(self, None, Global.download_manager)
@@ -56,46 +57,73 @@ class MainScreen(Screen):
         self.app.scheduler.schedule_synchronous(timedelta(minutes=10), self.load_weather)
         self.children.append(self.weather_widgets)
 
-        lh3 = Line(self, Line.Orientation.HORIZONTAL)
-        lh3.rectangle = Rectangle(self.weather_widgets.position(Anchor.TOP_LEFT).anchored(Anchor.BOTTOM_LEFT),
-                                  Size(self.width, lh3.preferred_size().height))
-        self.children.append(lh3)
+        temp_font = Font(size=11, bold=True)
+        self.out_temp = TextWidget(self, 'Out: -00.0° - -00.0°', temp_font)
+        self.out_temp.rectangle = Rectangle(self.weather_widgets.position(Anchor.TOP_LEFT).anchored(Anchor.BOTTOM_LEFT)
+                                            + Point(0, -1),
+                                            self.out_temp.preferred_size + Size(0, 2))
+        self.children.append(self.out_temp)
 
-        mp = MediaPlayer(self.app)
-        tt = MediaPlayerTrackTitleWidget(self, mp, Font(size=14))
-        tt.rectangle = Rectangle(lh3.position(Anchor.TOP_LEFT).anchored(Anchor.BOTTOM_LEFT),
-                                 Size(self.width, tt.font.font_extents().height))
-        self.children.append(tt)
+        self.balcony_temp = TextWidget(self, 'B: -00.0°', temp_font)
+        self.balcony_temp.rectangle = Rectangle(AnchoredPoint(self.width, self.out_temp.top, Anchor.TOP_RIGHT),
+                                                self.balcony_temp.preferred_size)
+        self.children.append(self.balcony_temp)
 
-        ttf = Font(size=11)
-        tp = MediaPlayerTrackPositionWidget(self, mp, ttf)
-        tp.rectangle = Rectangle(tt.position(Anchor.TOP_LEFT).anchored(Anchor.BOTTOM_LEFT),
-                                 Size(self.width / 3, tp.font.font_extents().height))
-        self.children.append(tp)
+        self.in_temp = TextWidget(self, 'In: -00.0° - -00.0°', temp_font)
+        self.in_temp.rectangle = Rectangle(AnchoredPoint((self.out_temp.right + self.balcony_temp.left) / 2,
+                                                         self.out_temp.top, Anchor.TOP_CENTER),
+                                           self.in_temp.preferred_size)
+        self.children.append(self.in_temp)
 
-        td = MediaPlayerTrackDurationWidget(self, mp, ttf)
-        td.rectangle = Rectangle(tt.position(Anchor.TOP_CENTER).anchored(Anchor.BOTTOM_CENTER),
-                                 Size(self.width / 3, td.font.font_extents().height))
-        td.h_alignment = TextWidget.HAlignment.CENTER
-        self.children.append(td)
+        Global.download_manager.get('https://klierlinge.de/log/values.php', self.load_klierlinge_values,
+                                    timedelta(seconds=29))
 
-        tr = MediaPlayerTrackRemainingWidget(self, mp, ttf)
-        tr.rectangle = Rectangle(tt.position(Anchor.TOP_RIGHT).anchored(Anchor.BOTTOM_RIGHT),
-                                 Size(self.width / 3, tr.font.font_extents().height))
-        tr.h_alignment = TextWidget.HAlignment.RIGHT
-        self.children.append(tr)
+        self.lh3 = Line(self, Line.Orientation.HORIZONTAL)
+        self.lh3.rectangle = Rectangle(self.out_temp.position(Anchor.TOP_LEFT).anchored(Anchor.BOTTOM_LEFT)
+                                       + Point(0, -1),
+                                       Size(self.width, self.lh3.preferred_size().height))
+        self.children.append(self.lh3)
 
-        aa = MediaPlayerAlbumArt(self, mp, Anchor.BOTTOM_RIGHT)
-        aa.rectangle = Rectangle(lh1.position(Anchor.BOTTOM_LEFT).anchored(Anchor.TOP_LEFT) + Point(0, 1),
-                                 lh1.position(Anchor.BOTTOM_LEFT) - tr.position(Anchor.TOP_RIGHT) - Size(0, 3))
-        self.children.append(aa)
+        self.media_player = MediaPlayer(self.app)
+        self.track_title = MediaPlayerTrackTitleWidget(self, self.media_player, Font(size=14))
+        self.track_title.rectangle = Rectangle(self.lh3.position(Anchor.TOP_LEFT).anchored(Anchor.BOTTOM_LEFT),
+                                               Size(self.width, self.track_title.font.font_extents().height))
+        self.children.append(self.track_title)
 
-        lh2 = Line(self, Line.Orientation.HORIZONTAL)
-        lh2.rectangle = Rectangle(tp.position(Anchor.TOP_LEFT).anchored(Anchor.BOTTOM_LEFT) + Point(0, -1),
-                                  Size(lv2_3.left, lh2.preferred_size().height))
-        self.children.append(lh2)
+        track_position_font = Font(size=11)
+        self.track_position = MediaPlayerTrackPositionWidget(self, self.media_player, track_position_font)
+        self.track_position.rectangle = Rectangle(
+            self.track_title.position(Anchor.TOP_LEFT).anchored(Anchor.BOTTOM_LEFT),
+            Size(self.width / 3, self.track_position.font.font_extents().height))
+        self.children.append(self.track_position)
 
-        lv2_3.set_height(lh2.bottom, VAnchor.TOP)
+        self.track_duration = MediaPlayerTrackDurationWidget(self, self.media_player, track_position_font)
+        self.track_duration.rectangle = Rectangle(
+            self.track_title.position(Anchor.TOP_CENTER).anchored(Anchor.BOTTOM_CENTER),
+            Size(self.width / 3, self.track_duration.font.font_extents().height))
+        self.track_duration.h_alignment = TextWidget.HAlignment.CENTER
+        self.children.append(self.track_duration)
+
+        self.track_remaining = MediaPlayerTrackRemainingWidget(self, self.media_player, track_position_font)
+        self.track_remaining.rectangle = Rectangle(
+            self.track_title.position(Anchor.TOP_RIGHT).anchored(Anchor.BOTTOM_RIGHT),
+            Size(self.width / 3, self.track_remaining.font.font_extents().height))
+        self.track_remaining.h_alignment = TextWidget.HAlignment.RIGHT
+        self.children.append(self.track_remaining)
+
+        self.album_art = MediaPlayerAlbumArt(self, self.media_player, Anchor.BOTTOM_RIGHT)
+        self.album_art.rectangle = Rectangle(
+            self.lh1.position(Anchor.BOTTOM_LEFT).anchored(Anchor.TOP_LEFT) + Point(0, 1),
+            self.lh1.position(Anchor.BOTTOM_LEFT) - self.track_remaining.position(Anchor.TOP_RIGHT) - Size(0, 3))
+        self.children.append(self.album_art)
+
+        self.lh2 = Line(self, Line.Orientation.HORIZONTAL)
+        self.lh2.rectangle = Rectangle(
+            self.track_position.position(Anchor.TOP_LEFT).anchored(Anchor.BOTTOM_LEFT) + Point(0, -1),
+            Size(self.lv2_3.left, self.lh2.preferred_size().height))
+        self.children.append(self.lh2)
+
+        self.lv2_3.set_height(self.lh2.bottom, VAnchor.TOP)
 
     def on_key_down(self, key: G19Key):
         if super().on_key_down(key):
@@ -106,3 +134,27 @@ class MainScreen(Screen):
 
     def load_weather(self, _=None) -> Optional[List[WeatherPeriod]]:
         return self.wetter_com.load_weather(lambda wps2: self.weather_widgets.set_weather_periods(wps2))
+
+    def load_klierlinge_values(self, values: bytes):
+        data = json.loads(values.decode('UTF-8'))
+        temp_out: List[float] = []
+        temp_out_date = [datetime.now()]
+        self._add_klierlinge_value(data, '062419C2687B.TEMP2', temp_out, temp_out_date)
+        self._add_klierlinge_value(data, '0674ED7469BD.TEMP', temp_out, temp_out_date)
+        self._add_klierlinge_value(data, '0674ED7469BD.TEMP2', temp_out, temp_out_date)
+        temp_in: List[float] = []
+        temp_in_date = [datetime.now()]
+        self._add_klierlinge_value(data, '030A84D918DC.TEMP', temp_in, temp_in_date)
+        self._add_klierlinge_value(data, '031B99C87CEE.TEMP', temp_in, temp_in_date)
+        self._add_klierlinge_value(data, '032602645A7F.TEMP', temp_in, temp_in_date)
+        self._add_klierlinge_value(data, 'PI_TEMP', temp_in, temp_in_date)
+        self.out_temp.text = 'Out: {:2.1f}° - {:2.1f}°'.format(min(temp_out), max(temp_out))
+        self.in_temp.text = 'In: {:2.1f}° - {:2.1f}°'.format(min(temp_in), max(temp_in))
+        self.balcony_temp.text = 'B: {:2.1f}°'.format(float(data['062419C2687B.TEMP']['Value']))
+
+    @staticmethod
+    def _add_klierlinge_value(data: Dict[str, Dict[str, Any]], name: str, values: List[Any], max_age: List[datetime]):
+        values.append(float(data[name]['Value']))
+        date = datetime.strptime(data[name]['Time'], '%Y-%m-%d %H:%M:%S')
+        if max_age[0] > date:
+            max_age[0] = date
