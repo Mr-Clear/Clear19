97a16287f97a42e73d89ea994cfe34c36702855d
Thomas <git@thomas.klierlinge.de>
2022-06-21 21:19:31 +0200

    Add menu to choose log level

--- clear19/App/menu_screen.py
+++ clear19/App/menu_screen.py
@@ -1,11 +1,33 @@
+import collections
+import logging
+
 from clear19.widgets.menu_widget import MenuWidget, MenuWidgetEntry
 from clear19.widgets.text_widget import Font
 from clear19.widgets.widget import Screen, AppWidget
+
+log = logging.getLogger(__name__)
+
+
+def set_log_level(level: int):
+    log.print(f"Setting log level to {logging.getLevelName(level)} ({level})")
+    logging.getLogger().setLevel(level)
 
 
 class MenuScreen(Screen):
     def __init__(self, parent: AppWidget):
         super().__init__(parent, "Menu")
 
-        m = MenuWidget(self, [MenuWidgetEntry("Exit", lambda _: self.app.exit())], Font())
-        m.rectangle = self.rectangle
+        self.menu_widget = MenuWidget(self, [MenuWidgetEntry("Exit", lambda _: self.app.exit()),
+                                             MenuWidgetEntry("Set Log Level", self._set_log_level)],
+                                      Font())
+        self.menu_widget.rectangle = self.rectangle
+
+    def _set_log_level(self, _):
+        menu = []
+        index = 0
+        levels = collections.OrderedDict(sorted(logging._levelToName.items()))
+        for i, (level, name) in enumerate(levels.items()):
+            menu.append(MenuWidgetEntry(name, lambda _, l=level: set_log_level(l)))
+            if level == logging.getLogger().getEffectiveLevel():
+                index = i
+        self.menu_widget.enter_submenu(menu, index)
--- clear19/widgets/menu_widget.py
+++ clear19/widgets/menu_widget.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import logging
 from dataclasses import dataclass
-from typing import Callable, List
+from typing import Callable, List, Optional, Tuple
 
 from clear19.logitech.g19 import G19Key, DisplayKey
 from clear19.widgets.geometry import Anchor, AnchoredPoint, Rectangle
@@ -16,7 +16,8 @@ log = logging.getLogger(__name__)
 class MenuWidgetEntry:
     text: str
     on_click: Callable[[MenuWidgetEntry], None]
-    widget: TextWidget = None
+    widget: Optional[TextWidget] = None
+    children: Optional[List[MenuWidgetEntry]] = None
 
 
 class MenuWidget(ContainerWidget):
@@ -25,35 +26,43 @@ class MenuWidget(ContainerWidget):
     """
     _menu: List[MenuWidgetEntry]
     _font: Font
-    _current_entry: int
-    _widget: TextWidget = None
+    _current_index: int
+    _widget: Optional[TextWidget] = None
+    _menu_stack: [List[Tuple[List[MenuWidgetEntry, int]]]] = []
 
-    def __init__(self, parent: ContainerWidget, menu: List[MenuWidgetEntry], font: Font, current_entry: int = 0):
+    def __init__(self, parent: ContainerWidget, menu: List[MenuWidgetEntry], font: Font, current_index: int = 0):
         super().__init__(parent)
         self._menu = menu
         self._font = font
-        self._current_entry = current_entry
+        self._current_index = current_index
         self.build_menu()
 
     def build_menu(self):
         self.children.clear()
         y = 0
-        for i in range(len(self.menu)):
-            entry = self.menu[i]
+        for i in range(len(self._menu)):
+            entry = self._menu[i]
             a = TextWidget(self, entry.text, self.font)
             a.rectangle = Rectangle(AnchoredPoint(0, y, Anchor.TOP_LEFT), a.preferred_size)
-            if i == self.current_entry:
+            if i == self._current_index:
                 a.foreground = self.background
                 a.background = self.foreground
             entry.widget = a
+            y += a.preferred_size.height
 
-    @property
-    def menu(self) -> List[MenuWidgetEntry]:
-        return self._menu
+    def _update(self):
+        self.build_menu()
+        self.dirty = True
 
-    @menu.setter
-    def menu(self, menu: List[MenuWidgetEntry]):
+    def enter_submenu(self, menu: List[MenuWidgetEntry], index: int = 0):
+        self._menu_stack.append((self._menu, self._current_index))
         self._menu = menu
+        self._current_index = index
+        self._update()
+
+    def leave_submenu(self):
+        self._menu, self._current_index = self._menu_stack.pop()
+        self._update()
 
     @property
     def font(self) -> Font:
@@ -63,18 +72,28 @@ class MenuWidget(ContainerWidget):
     def font(self, font: Font):
         self._font = font
 
-    @property
-    def current_entry(self) -> int:
-        return self._current_entry
-
-    @current_entry.setter
-    def current_entry(self, current_entry: int):
-        self._current_entry = current_entry
-
     def on_key_down(self, key: G19Key) -> bool:
-        entry = self.menu[self.current_entry]
+        entry = self._menu[self._current_index]
         if key == DisplayKey.OK:
             log.info(f'Selected menu entry "{entry.text}"')
             entry.on_click(entry)
             return True
+        if key == DisplayKey.DOWN:
+            self._current_index += 1
+            if self._current_index >= len(self._menu):
+                self._current_index = 0
+            self._update()
+            return True
+        if key == DisplayKey.UP:
+            self._current_index -= 1
+            if self._current_index < 0:
+                self._current_index = len(self._menu) - 1
+            self._update()
+            return True
+        if key == DisplayKey.BACK:
+            if self._menu_stack:
+                self.leave_submenu()
+                return True
+            else:
+                return False
         return False
